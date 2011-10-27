bc40a5484feb48366d41a3f3bdbb23ef67aea423
Kenny Millington <kenny@kennynet.co.uk>
2011-10-27 20:28:45 +0100

    Add --with-static configure switch to build unruu statically.

--- autogen.sh
+++ autogen.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 if [ "$1" == "clean" ]; then
     rm -f Makefile Makefile.in configure aclocal.m4
-    rm -f missing depcomp install-sh
+    rm -f missing depcomp install-sh  config.guess config.sub ltmain.sh
+    find m4/ -lname \* -exec rm -f {} \;
 else
     autoreconf -fsi
 fi
